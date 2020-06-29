a65e0919895de679e27a28f0470a3af124911412
benwschulz <benwschulz@gmail.com>
2020-06-29 14:40:28 -0500

    Added more solution files and gifs

---
+++ FlashDetector/Gif.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace FlashDetector
+{
+    public class Gif
+    {
+        public Image data;
+
+        public Gif(string path)
+        {
+            data = Image.FromFile(path);
+        }
+    }
+}
--- FlashDetector/Program.cs
+++ FlashDetector/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace FlashDetector
+﻿using System.IO;
+
+namespace FlashDetector
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string directory = @"C:\Users\benws\source\repos\FlashDetector\FlashDetector\Gifs";
+            var gifs = Directory.GetFiles(directory);
+
+            var foo = new Gif(gifs[0]);
         }
     }
 }
